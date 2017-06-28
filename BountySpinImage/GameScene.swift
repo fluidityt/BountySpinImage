233e01ba4f17c5f0a81f5a17c4918f55b7ab65b1
fluidityt <hackbook17@justins-macbook-pro.local>
2017-06-28 01:25:16 -0400

    zz

--- BountySpinImage/GameScene.swift
+++ BountySpinImage/GameScene.swift
@@ -22,72 +22,65 @@ import GameplayKit
  }
  */
 
-class GameScene: SKScene {
-  
-  enum Direction { case left, right }
+    class GameScene: SKScene {
+      
+      // Left spin is ascending indices, right spin is descending indices:
+      var initialTextures = [SKTexture]()
+      
+      // Reset then reload this from 0-6 with the correct image sequences from initialTextures:
+      var nextTextures = [SKTexture]()
+      
+      var sprite = SKSpriteNode()
+      
+      // Use gesture recognizer or other means to set how fast the spin should be.
+      var velocity = TimeInterval(0.1)
+      
+      enum Direction { case left, right }
+      
+      func spin(direction: Direction, timePerFrame: TimeInterval) {
+        
+        nextTextures = []
+        for _ in 0...6 {
 
-  var oldTextures = [SKTexture]()
-  var textures = [SKTexture]()
-  var sprite = SKSpriteNode()
-  var spinning = Direction.left
-  
-  func spinLeft(timePerFrame: TimeInterval) {
-    // Left is ascending
-    
-    let oldTextures = textures
-    textures = []
-    
-    for _ in 0...6 {
+          var index = initialTextures.index(of: sprite.texture!)
+          
+          // Left is ascending, right is descending:
+          switch direction {
+          case .left:
+            if index == (initialTextures.count - 1) { index = 0 } else { index! += 1 }
+          case .right:
+            if index == 0 { index = (initialTextures.count - 1) } else { index! -= 1 }
+          }
+          
+          let nextTexture = initialTextures[index!]
+          nextTextures.append(nextTexture)
+          sprite.texture = nextTexture
+        }
+        
+        let action = SKAction.repeatForever(.animate(with: nextTextures, timePerFrame: timePerFrame))
+        sprite.run(action)
+      }
       
-      var index = oldTextures.index(of: sprite.texture!)
-      if index == (oldTextures.count - 1) { index = 0 } else { index! += 1 }
+      override func didMove(to view: SKView) {
+        removeAllChildren()
+        
+        // Make our textures for spinning:
+        for i in 0...6 {
+          initialTextures.append(SKTexture(imageNamed: "img_\(i)"))
+        }
+        nextTextures = initialTextures
+        
+        sprite.texture = nextTextures.first!
+        sprite.size = nextTextures.first!.size()
+        addChild(sprite)
+        spin(direction: .left, timePerFrame: 0.10)
+      }
       
-      let nextTexture = oldTextures[index!]
-      textures.append(nextTexture)
-      sprite.texture = nextTexture
+      override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        spin(direction: .right, timePerFrame: velocity)
+      }
+      
+      override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        spin(direction: .left, timePerFrame: velocity)
+      }
     }
-    
-    let action = SKAction.repeatForever(.animate(with: textures, timePerFrame: timePerFrame))
-    sprite.run(action)
-  }
-  
-  func spinRight(timePerFrame: TimeInterval) {
-    // Right is descending
-    
-    let oldTextures = textures
-    textures = []
-    
-    for _ in 0...6 {
-      
-      var index = oldTextures.index(of: sprite.texture!)
-      if index == 0 { index = (oldTextures.count - 1) } else { index! -= 1 }
-      
-      let nextTexture = oldTextures[index!]
-      textures.append(nextTexture)
-      sprite.texture = nextTexture
-    }
-    
-    let action = SKAction.repeatForever(.animate(with: textures, timePerFrame: timePerFrame))
-    sprite.run(action)
-  }
-  
-  override func didMove(to view: SKView) {
-    removeAllChildren()
-    
-    for i in 0...6 {
-      textures.append(SKTexture(imageNamed: "img_\(i)"))
-    }
-    sprite.texture = textures.first!
-    sprite.size = textures.first!.size()
-    addChild(sprite)
-    spinLeft(timePerFrame: 0.25)
-  }
-  
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    spinRight(timePerFrame: 0.25)
-  }
-  
-  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-    spinLeft(timePerFrame: 0.25)
-  }
-}
