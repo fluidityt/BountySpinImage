1fa39f8762ffd4e31ed95031febc83d95d3bd47b
fluidityt <fluidityt@users.noreply.github.com>
2017-06-29 01:32:12 -0400

    Update GameScene2.swift

--- BountySpinImage/GameScene2.swift
+++ BountySpinImage/GameScene2.swift
@@ -97,6 +97,9 @@ extension GameScene2 {
     didClicklabel = false
   }
   override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+   
+    if didClickLabel { return }  
+	  
     let touch = touches.first!
     
     let dx: Direction
